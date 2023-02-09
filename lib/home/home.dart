5a83cfc6b6c26555407934e9787756464683cda1
le <gemygemy31@yahoo.com>
2023-02-09 18:01:08 +0200

    initial project of islami

---
+++ lib/home/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String Route_Name= 'home';
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'home/home.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeScreen.Route_Name : (context) => HomeScreen(),
+      },
+      initialRoute: HomeScreen.Route_Name,
+    );
+  }
+}
